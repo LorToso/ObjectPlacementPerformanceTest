e6bf509417350eee7c12ec0c3f5b9139e7e4aea7
LorToso <LorToso@gmail.com>
2015-02-25 12:46:45 +0100

    Some fixes

--- ObjectPlacementPerformanceTest/Actor.cs
+++ ObjectPlacementPerformanceTest/Actor.cs
@@ -4,11 +4,41 @@ namespace ObjectPlacementPerformanceTest
 {
 	public class Actor
 	{
-		private int x { get; set; }
-		private int y { get; set; }
+		private int x;
+
+		public int X {
+			get {
+				return x;
+			}
+		}
+
+		private int y;
+
+		public int Y {
+			get {
+				return y;
+			}
+		}
+
+		private int width;
+
+		public int Width {
+			get {
+				return width;
+			}
+		}
+
+		private int height;
+
+		public int Height {
+			get {
+				return height;
+			}
+		}
+
 		private int id;
 
-		public int getId {
+		public int Id {
 			get {
 				return id;
 			}
@@ -16,8 +46,15 @@ namespace ObjectPlacementPerformanceTest
 
 		private static int idCount = 0;
 
-		public Actor()
+		public Actor(World world)
+			:this (world.NWidth, world.NHeight)
 		{
+		}
+		public Actor(int worldWidth, int worldHeight)
+		{
+			Random random = new Random ();
+			width = random.Next (worldWidth/2);
+			height = random.Next (worldHeight/2);
 			id = idCount;
 			idCount++;
 		}
--- ObjectPlacementPerformanceTest/Program.cs
+++ ObjectPlacementPerformanceTest/Program.cs
@@ -1,34 +1,69 @@
 using System;
+using System.Diagnostics;
 
 namespace ObjectPlacementPerformanceTest
 {
 	class MainClass
 	{
+
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Start!");
 
 			int nObjectCount = 1000;
-			int nWidth = 1000;
-			int nHeight = 1000;
+			int nWidth = 100;
+			int nHeight = 100;
 
 
-			ObjectContainer matrixModel = new MatrixModel (nWidth, nHeight);
-			ObjectContainer mapModel = new MapModel (nWidth, nHeight);
+			World matrixModel = new MatrixModel (nWidth, nHeight);
+			World mapModel = new MapModel (nWidth, nHeight);
 
-			Console.WriteLine ("Adding " + nObjectCount + " Objects!");
-
+			
+			Random random = new Random ();
 			for (int i=0; i < nObjectCount; i++) {
-				Actor actor = new Actor ();
-				int x = new Random ().Next (nWidth);
-				int y = new Random ().Next (nHeight);
+				Actor actor = new Actor (nWidth, nHeight);
+				int x = random.Next (nWidth);
+				int y = random.Next (nHeight);
 				matrixModel.addObject (actor, x, y);
 				mapModel.addObject (actor, x, y);
+				Console.WriteLine ("Adding Object " + actor.Id + "at (" + x + "|" + y + "). Size + " + actor.Width + "x" + actor.Height);
 			}
 
+			int executionTimes = 100;
 
+			Console.Out.WriteLine ("Starting Tests!");
+			Console.Out.WriteLine ("World Size: " + nWidth + "x" + nHeight);
+			Console.Out.WriteLine ("ActorCount: " + nObjectCount);
+			Console.Out.WriteLine ("ExecutionTimes: " + executionTimes);
+			Console.Out.WriteLine ("MapModel:");
 
-			System.Threading.Thread.Sleep (5000);
+			Stopwatch stopWatch = new Stopwatch ();
+			stopWatch.Start ();
+
+			for (int i = 0; i < executionTimes; i++) {
+				mapModel.testSinglePoint ();
+				mapModel.testRect ();
+				mapModel.testId (nObjectCount/2);
+			}
+			stopWatch.Stop ();
+			
+			Console.Out.WriteLine ("Elapsed Time in ms: " + stopWatch.ElapsedMilliseconds);
+			stopWatch.Reset ();
+			
+			Console.Out.WriteLine ("MatixModel:");
+			stopWatch.Start ();
+
+			for (int i = 0; i < executionTimes; i++) {
+				matrixModel.testSinglePoint ();
+				matrixModel.testRect ();
+				matrixModel.testId (nObjectCount/2);
+			}
+			stopWatch.Stop ();
+
+			Console.Out.WriteLine ("Elapsed Time in ms: " + stopWatch.ElapsedMilliseconds);
+			stopWatch.Reset ();
+
+			System.Threading.Thread.Sleep (10000);
 		}
 	}
 }
